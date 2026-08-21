d0a30f6bda13c07df66720bae05a8c2d48d7a232
Vu <lenguyenanhvu227@gmail.com>
2026-08-21 13:12:19 +0700

    sd

--- ProjectTest/TestPro1.cs
+++ ProjectTest/TestPro1.cs
@@ -5,7 +5,17 @@
         Console.WriteLine("Hello, World!");
         Console.Write("Hi");
         Console.WriteLine(", I am World");
-        Console.WriteLine("This is the first line");
-        Console.WriteLine("This is the second line");
+        Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+        Console.WriteLine("Invoice: 1021\t\tComplete!");
+        Console.WriteLine("Invoice: 1022\t\tComplete!");
+        Console.Write("\nOutput Directory:\t");
+        var Anhdudu = "dugva";
+        Console.WriteLine(Anhdudu);
+        Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+        string projectName = "ACME";
+        Console.WriteLine($"View English output:\n \t c:\\exercise\\{projectName}\\data.txt\n");
+        decimal thuongso = 1/3m;
+        Console.WriteLine(thuongso);
     }
 }
